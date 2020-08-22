abef1287e28560ce8eb051ae061aa9e35f073573
HucksApp <jakehunt000@gmail.com>
2020-08-22 04:46:09 +0100

    no 1

--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -1,6 +1,6 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-import {useSpring, animated} from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 
 
 import MenuTwoToneIcon from '@material-ui/icons/MenuTwoTone';
@@ -15,49 +15,53 @@ import '../Styles/top_nav.css'
 
 
 
-export default function TopNav(props){
+export default function TopNav(props) {
 
-const [toggle, set] = useState(false)
-const prop = usepring({
-                    to: async (next, cancel) => {
-                    await next({opacity: 0.5})
-                    await next({opacity: 0.1})
-                },
-                from: {opacity: 0,color:"white"}
-})
-
-
+    const [toggle, set] = useState(false)
+    const prop = useSpring({
+        to: async (next, cancel) => {
+            await next({ opacity: 0.5 })
+            await next({ opacity: 0.1 })
+        },
+        from: { opacity: 0, color: "white" }
+    })
 
 
 
 
-    const [ state , setState ] = useState( false );
 
-    const toggleDrawer = (openOrClose)=>{
-       
-      setState( openOrClose ) 
-       set(openOrClose)
+
+    const [state, setState] = useState(false);
+
+    const toggleDrawer = (openOrClose) => {
+
+        setState(openOrClose)
+        set(openOrClose)
     }
     let btn;
 
-    if(!state){
-        btn = <animated.div style={prop}><MenuTwoToneIcon style={{color:props.color_type,fontSize: "60px"}} onClick={()=>toggleDrawer(true)}/></animated.div>
-    }else if(state){
-                        
-        btn = <animated.div style={prop}><CloseIcon style={{color:props.color_type,fontSize: "60px"}} /></animated.div>
-                    }
+    if (!state) {
+        btn = <animated.div style={prop}>
+                <MenuTwoToneIcon style={{ color: props.color_type, fontSize: "60px" }} onClick={() => toggleDrawer(true)} />
+            </animated.div>
+    } else if (state) {
+
+        btn = <animated.div style={prop}>
+                <CloseIcon style={{ color: props.color_type, fontSize: "60px" }} />
+            </animated.div>
+    }
 
 
 
-    return(
+    return (
 
-        <div className = "top_nav">
-            <NavDrawer toggle={state} chaToggle = {toggleDrawer} />
+        <div className="top_nav">
+            <NavDrawer toggle={state} chaToggle={toggleDrawer} />
             <div>
-            {btn}
+                {btn}
             </div>
-            
-            <HucksLogo/>
+
+            <HucksLogo />
 
         </div>
 
