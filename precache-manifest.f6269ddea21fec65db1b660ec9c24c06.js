829e50adeb4d80e8215ce3f109d182359c54692f
HucksApp <jakehunt000@gmail.com>
2020-09-19 16:17:31 +0100

    Updates

---
+++ precache-manifest.f6269ddea21fec65db1b660ec9c24c06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c908162d09032f37c6b9e5456c98f82a",
+    "url": "/hucksWare/index.html"
+  },
+  {
+    "revision": "f9f78a1428bfa5e328d9",
+    "url": "/hucksWare/static/css/2.00129a64.chunk.css"
+  },
+  {
+    "revision": "afb43d61f60bbbd76dd8",
+    "url": "/hucksWare/static/css/main.f1cf9e42.chunk.css"
+  },
+  {
+    "revision": "f9f78a1428bfa5e328d9",
+    "url": "/hucksWare/static/js/2.4e8e257a.chunk.js"
+  },
+  {
+    "revision": "20da4b7404df1784142b55ccd4845ed1",
+    "url": "/hucksWare/static/js/2.4e8e257a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb43d61f60bbbd76dd8",
+    "url": "/hucksWare/static/js/main.e7bdac70.chunk.js"
+  },
+  {
+    "revision": "6a3273dcccc3e96277f3",
+    "url": "/hucksWare/static/js/runtime-main.bc28fcb7.js"
+  },
+  {
+    "revision": "ccd2926b176eb5cf2c2ee638ec1d0aea",
+    "url": "/hucksWare/static/media/code.ccd2926b.gif"
+  },
+  {
+    "revision": "50a024cb5a8906e8619a4111eeeb6496",
+    "url": "/hucksWare/static/media/feshcom.50a024cb.svg"
+  },
+  {
+    "revision": "1841505208db4d4c8abd87cc2b03b8b4",
+    "url": "/hucksWare/static/media/hucks22.18415052.png"
+  },
+  {
+    "revision": "f0f19a9837cd862ef1037d6d6b0c693c",
+    "url": "/hucksWare/static/media/kp.f0f19a98.svg"
+  },
+  {
+    "revision": "e47dfcb05f30c2268ced1bb6a8ccb064",
+    "url": "/hucksWare/static/media/periscope.e47dfcb0.svg"
+  }
+]);
