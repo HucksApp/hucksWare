7e5549f9e16a53e54a24c147e96ec77609a3200f
HucksApp <jakehunt000@gmail.com>
2020-09-21 17:43:50 +0100

    n

--- src/Components/Hacker.jsx
+++ src/Components/Hacker.jsx
@@ -25,7 +25,7 @@ const Hacker = () => {
           <Paper elevation={5}>
             <article style={{ color: '#616161' }}>
               Hacking is a calling , I can't reject.
-              I have long years of experience in Kali-Linux and Kali tools,..Wire Shark, Nmap , Rapid7 frameworks, AirCrack-ng.Creation of Exploitaion Sofwares,Malware,Wireless Hacking, Social Engineering, Reverse Engineering....I love to break the rules,I feel rules are not for the curious.What is the Benefit of life when You cant Explore it,Though I only hack What i Have Permission to hack.              
+              I have long years of experience in Kali-Linux and Kali tools,..Wire Shark, Nmap , Rapid7 frameworks, AirCrack-ng.Creation of Exploitaion Sofwares,Malware,Wireless Hacking, Social Engineering, Reverse Engineering....I love to break the rules,I feel rules are not for the curious.What is the benefit of life when You cant explore it,Though I only hack What i have permission to hack.              
         <div style={{ color: '#aa84e0' }}>What Would You Do when You Have a Professional Ethical Hacker at Your Disposal.</div>
             </article>
           </Paper>
