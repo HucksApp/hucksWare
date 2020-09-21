b75a70331693fd0c3a0ba9741b1b75fbbcebf3bc
HucksApp <jakehunt000@gmail.com>
2020-09-21 19:57:52 +0100

    n

--- src/Components/NavDrawer.jsx
+++ src/Components/NavDrawer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import WorkIcon from '@material-ui/icons/Work';
-import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import BallotIcon from '@material-ui/icons/Ballot';
 
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -18,7 +18,7 @@ import '../Styles/nav_drawer.css'
 
 export default function NavDrawer(props) {
 
-    const BioLink = (props) => {
+    const ResumeLink = (props) => {
 
         return < NavLink to={'/hucksWare'} {...props} />;
 
@@ -37,15 +37,15 @@ export default function NavDrawer(props) {
 
         <Drawer elevation={20} variant='temporary' anchor={"left"} open={props.toggle} onClose={() => props.chaToggle(false)}>
             <List style={{ color: '#616161', fontFamily: 'Raleway' }} >
-                <ListItem button component={BioLink} >
-                    <ListItemIcon> <AccessibilityIcon /> </ListItemIcon>
-                    <ListItemText primary={"BIO"} />
+                <ListItem button component={ResumeLink} >
+                    <ListItemIcon> <AssignmentIcon /> </ListItemIcon>
+                    <ListItemText primary={"RESUME"} />
                 </ListItem >
 
                 <Divider variant={'middle'} />
 
                 <ListItem button component={ProjectLink} >
-                    <ListItemIcon> <WorkIcon /> </ListItemIcon>
+                    <ListItemIcon> <BallotIcon /> </ListItemIcon>
                     <ListItemText primary={"PROJECTS"} />
                 </ListItem>
 
